15c53ec8acba7fcfc57f4755e8f12015e6464b0a
angelinetipa <angelinetipa@gmail.com>
2023-04-16 21:21:21 +0800

    check each character

--- Decryptor.py
+++ Decryptor.py
@@ -3,6 +3,7 @@ print("*" * 70)
 encrypted_str = input("\n\033[3m\033[1m" "Enter a text to decrypt: " "\033[0m" ) 
 decrypted_str = ""
 # check each character
+for i in range(len(encrypted_str)):
 # if *, change to a
 # if &, change to e
 # if #, change to i
