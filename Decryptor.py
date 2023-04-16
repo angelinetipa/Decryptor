cbfccc7c0a3c09c3c702e8d4dbc2247696ce038f
angelinetipa <angelinetipa@gmail.com>
2023-04-16 21:22:29 +0800

    if asterisk, change to a

--- Decryptor.py
+++ Decryptor.py
@@ -4,7 +4,9 @@ encrypted_str = input("\n\033[3m\033[1m" "Enter a text to decrypt: " "\033[0m" )
 decrypted_str = ""
 # check each character
 for i in range(len(encrypted_str)):
-# if *, change to a
+#   if *, change to a
+    if encrypted_str[i] == "*":
+        decrypted_str += "\033[0;31m" "a" "\033[0m"
 # if &, change to e
 # if #, change to i
 # if +, change to o
