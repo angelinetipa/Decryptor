692b6292a9100587e34cef7be4a9c4c3a3ac6602
angelinetipa <angelinetipa@gmail.com>
2023-04-16 21:18:08 +0800

    initial commit

---
+++ Decryptor.py
@@ -0,0 +1,9 @@
+# ask user for input
+# check each character
+# if *, change to a
+# if &, change to e
+# if #, change to i
+# if +, change to o
+# if !, change to u
+# else, do not change 
+# print output
