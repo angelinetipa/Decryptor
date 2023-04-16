d0b49e55690c6390eceb463927bd246f3a81dc59
angelinetipa <angelinetipa@gmail.com>
2023-04-16 21:24:07 +0800

    if ampersand, change to e

--- Decryptor.py
+++ Decryptor.py
@@ -7,7 +7,9 @@ for i in range(len(encrypted_str)):
 #   if *, change to a
     if encrypted_str[i] == "*":
         decrypted_str += "\033[0;31m" "a" "\033[0m"
-# if &, change to e
+#   if &, change to e
+    elif encrypted_str[i] == "&":
+        decrypted_str += "\033[0;31m" "e" "\033[0m"
 # if #, change to i
 # if +, change to o
 # if !, change to u
